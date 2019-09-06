a6d316825fd55ff3bb4c4f550cd098511a95cc5e
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-09-06 13:38:38 +0200

    reformat code

--- src/renderer/components/pages/SettingsPage.tsx
+++ src/renderer/components/pages/SettingsPage.tsx
@@ -7,10 +7,10 @@ import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button/Button'
 import makeStyles from '@material-ui/core/styles/makeStyles'
+import { FormHelperText } from '@material-ui/core'
 
 import { useBackend } from '../../hooks/backend'
 import { useConfig } from '../../hooks/config'
-import { FormHelperText } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
     button: {
