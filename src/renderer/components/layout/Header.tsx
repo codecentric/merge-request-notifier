f918b7d29751e98de029d4a5fcc2dfa8881d3c50
jkempff <kempff@ju.am>
2019-09-17 14:53:37 +0200

    A little more compact, slightly different header and footer, no more scrollbar, mini improvements

--- src/renderer/components/layout/Header.tsx
+++ src/renderer/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Box, Heading } from 'rebass'
 
 export const Header = () => (
-    <Box p={2} bg='white' sx={{ borderBottom: '1px solid', overflow: 'hidden', borderColor: 'shadow' }}>
+    <Box p={2} variant='barGradient' sx={{ borderBottom: '1px solid', overflow: 'hidden', borderColor: 'shadow' }}>
         <Heading textAlign='center' fontWeight='bold' fontSize={2}>
             Merge Requests
         </Heading>
