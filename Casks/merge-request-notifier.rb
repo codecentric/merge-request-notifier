7036b3ad5f7be30203229661f124980b8da711f7
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-09-21 23:16:34 +0200

    add checksum

--- Casks/merge-request-notifier.rb
+++ Casks/merge-request-notifier.rb
@@ -1,6 +1,6 @@
 cask 'merge-request-notifier' do
   version '0.6.0'
-  sha256 :no_check
+  sha256 'c58b38a84f7f0c5570636a0ac3127b634bb055945fe5e02c9e7437993d5fa571'
 
   url "https://github.com/codecentric/merge-request-notifier/releases/download/v#{version}/Merge.Request.Notifier-#{version}.dmg"
   appcast 'https://github.com/codecentric/merge-request-notifier/releases.atom'
