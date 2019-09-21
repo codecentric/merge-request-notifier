f84a6bf2bb026a09535bd294ced42fa874fa77f7
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-09-21 23:01:26 +0200

    fix version number

--- Casks/merge-request-notifier.rb
+++ Casks/merge-request-notifier.rb
@@ -1,5 +1,5 @@
 cask 'merge-request-notifier' do
-  version 0.6.0
+  version '0.6.0'
   sha256 :no_check
 
   url 'https://github.com/codecentric/merge-request-notifier/releases/download/v#{version}/Merge.Request.Notifier-#{version}.dmg'
