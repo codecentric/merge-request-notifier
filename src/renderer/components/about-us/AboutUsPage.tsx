ca76b7fba1f25a6a67a2d260887cf62e3fa79269
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-11-05 22:46:10 +0100

    Thanks section

--- src/renderer/components/about-us/AboutUsPage.tsx
+++ src/renderer/components/about-us/AboutUsPage.tsx
@@ -2,9 +2,9 @@ import { remote, shell } from 'electron'
 import * as React from 'react'
 import { Box, Text } from 'rebass'
 
-const createIssue: React.MouseEventHandler<HTMLAnchorElement> = event => {
+const openUrl = (url: string): React.MouseEventHandler<HTMLAnchorElement> => event => {
     event.preventDefault()
-    shell.openExternal('https://github.com/codecentric/merge-request-notifier/issues/new')
+    shell.openExternal(url)
 }
 
 export const AboutUsPage = () => {
@@ -12,17 +12,32 @@ export const AboutUsPage = () => {
 
     return (
         <Box p={2}>
-            <Text>
+            <Text fontSize='14px'>
                 This app was build because our team was searching for a tool to see the number of our current open merge requests and make them easily
                 accessible. We didn't found such a tool and so we decided to build one by our own ;-)
             </Text>
 
             <Text mt={3} fontWeight='bold'>
+                Thanks
+            </Text>
+            <Text fontSize='14px'>
+                We want to thank our employers (
+                <a href='#' onClick={openUrl('https://codecentric.de')}>
+                    codecentric
+                </a>{' '}
+                and{' '}
+                <a href='#' onClick={openUrl('https://uxi.de')}>
+                    UX&I
+                </a>
+                ) to make it possible to develop this app in our +1 time.
+            </Text>
+
+            <Text mt={3} fontWeight='bold'>
                 Issues / Feedback
             </Text>
-            <Text>
+            <Text fontSize='14px'>
                 You found an issue or missed some feature? We are very keen about your feedback and appreciate any help. Please{' '}
-                <a href='#' onClick={createIssue}>
+                <a href='#' onClick={openUrl('https://github.com/codecentric/merge-request-notifier/issues/new')}>
                     create an issue
                 </a>{' '}
                 on GitHub.
