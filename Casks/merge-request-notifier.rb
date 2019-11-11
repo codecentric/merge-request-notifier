d37cea7cadcbe190231aeea149b3ca672759ef48
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-11-11 15:58:01 +0100

    fix download link

--- Casks/merge-request-notifier.rb
+++ Casks/merge-request-notifier.rb
@@ -2,7 +2,7 @@ cask 'merge-request-notifier' do
   version '0.9.0'
   sha256 'e39281c8a2aaa862c6c66af198ff8fb9743dd5fcc24d594e81ee558b41944a34'
 
-  url "https://github.com/codecentric/merge-request-notifier/releases/download/v#{version}/Merge.Request.Notifier-#{version}.dmg"
+  url "https://github.com/codecentric/merge-request-notifier/releases/download/v#{version}/Merge-Request-Notifier-#{version}.dmg"
   appcast 'https://github.com/codecentric/merge-request-notifier/releases.atom'
   name 'Merge Request Notifier'
   homepage 'https://github.com/codecentric/merge-request-notifier'
