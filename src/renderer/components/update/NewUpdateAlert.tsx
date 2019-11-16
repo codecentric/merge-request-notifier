7f2b71332ab1c487b89456644cfb8deca45bdf1c
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-11-16 14:33:42 +0100

    decrease font size to 12

--- src/renderer/components/update/NewUpdateAlert.tsx
+++ src/renderer/components/update/NewUpdateAlert.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const NewUpdateAlert = () => {
     return (
         <Box py={1} px={2} sx={{ position: 'sticky', zIndex: 2, top: 0, borderBottom: '1px solid', borderColor: 'shadow' }} bg='siteBackground'>
-            <Text fontWeight='bold' color='red' letterSpacing={1} sx={{ textTransform: 'uppercase' }} fontSize={'14px'}>
+            <Text fontWeight='bold' color='red' letterSpacing={1} sx={{ textTransform: 'uppercase' }} fontSize={'12px'}>
                 <Flex justifyContent='space-between'>
                     <span>New update available</span>
                     <Link to='/update' title='Update details'>
