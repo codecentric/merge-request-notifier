f31367ca5113cb359b5f578b4a41e06a37e7e002
Carsten Rohrbach <carsten.rohrbach@codecentric.de>
2020-04-03 11:44:01 +0200

    On linux, open from tray at the tray position, open via shortcut centered on the screen where the mouse is

--- src/main/positioning/linux.ts
+++ src/main/positioning/linux.ts
@@ -1,13 +1,32 @@
-import { BrowserWindow, Tray } from 'electron'
+import { BrowserWindow, Tray, screen } from 'electron'
 
-export const linuxWindowPosition = (win: BrowserWindow, tray: Tray) => {
-    // TODO: Carsten please have a look and fix the position ;-)
-
+export const linuxWindowPosition = (win: BrowserWindow, tray: Tray, fromTray: boolean) => {
     const windowBounds = win.getBounds()
-    const trayBounds = tray.getBounds()
+    const mousePosition = screen.getCursorScreenPoint()
+    const { bounds: displayBounds } = screen.getDisplayNearestPoint(mousePosition)
 
-    const x = Math.round(trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2)
-    const y = Math.round(trayBounds.y + trayBounds.height + 4)
+    if (fromTray) {
+        const trayBounds = tray.getBounds()
 
-    return { x, y }
+        let x: number
+        let y: number
+        if (trayBounds.width > 0) {
+            x = Math.round(trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2)
+            y = Math.round(trayBounds.y + trayBounds.height + 4)
+        } else {
+            // no tray info
+            x = Math.round(mousePosition.x - windowBounds.width / 2)
+            y = Math.round(mousePosition.y + 4)
+        }
+
+        x = Math.min(x, displayBounds.x + displayBounds.width - windowBounds.width - 10)
+        y = Math.min(y, displayBounds.y + displayBounds.height - windowBounds.height - 20)
+
+        return { x, y }
+    } else {
+        // use centered window
+        const x = displayBounds.x + (displayBounds.width - windowBounds.width) / 2
+        const y = displayBounds.y + (displayBounds.height - windowBounds.height) / 2
+        return { x, y }
+    }
 }
