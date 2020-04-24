84ba31a06821f7f235e124945bab0edde1424b47
Matthias Rütten <matthias.ruetten@codecentric.de>
2020-04-24 17:04:23 +0200

    only show back button when settings are available

--- src/renderer/components/layout/Header.tsx
+++ src/renderer/components/layout/Header.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { Box, Flex, Text } from 'rebass'
 import { Link, Route, Switch } from 'react-router-dom'
-
 import ArrowIcon from '@material-ui/icons/ArrowBackSharp'
+
+import { useConfig } from '../../hooks/config'
 
 const BackLink = () => (
     <Box sx={{ position: 'absolute', left: 0, top: 0 }}>
@@ -15,13 +16,15 @@ const BackLink = () => (
 )
 
 export const Header: React.FunctionComponent = () => {
+    const connectionConfigDefined = !!useConfig().config.connectionConfig
+
     return (
         <Box p={2} variant='barGradient' sx={{ borderBottom: '1px solid', overflow: 'hidden', borderColor: 'shadow', color: 'textColor' }}>
             <Text textAlign='center' fontWeight='bold' fontSize={3}>
                 <Switch>
                     <Route path='/config'>
                         <Flex sx={{ position: 'relative' }}>
-                            <BackLink />
+                            {connectionConfigDefined && <BackLink />}
                             <Box flex='1 0 auto'>
                                 <Text>Settings</Text>
                             </Box>
@@ -37,7 +40,7 @@ export const Header: React.FunctionComponent = () => {
                     </Route>
                     <Route path='/about-us'>
                         <Flex sx={{ position: 'relative' }}>
-                            <BackLink />
+                            {connectionConfigDefined && <BackLink />}
                             <Box flex='1 0 auto'>
                                 <Text>About us</Text>
                             </Box>
