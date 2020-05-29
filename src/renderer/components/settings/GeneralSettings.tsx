f7bce4f43d8e3db333728d18d9c0c22e8fc0d04a
Matthias Rütten <matthias.ruetten@codecentric.de>
2020-05-29 13:09:41 +0200

    make the tray configurable

--- src/renderer/components/settings/GeneralSettings.tsx
+++ src/renderer/components/settings/GeneralSettings.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Box } from 'rebass'
+import { Label, Select } from '@rebass/forms'
 
 import { useConfig } from '../../hooks/config'
 import { CheckboxInput } from '../form/CheckboxInput'
@@ -57,33 +58,62 @@ export const GeneralSettings: React.FunctionComponent = () => {
                         onChange={updateCheckoxValue('disableWipNotifications')}
                     />
                 )}
-                <CheckboxInput label='Dark mode' id='darkMode' name='darkMode' defaultChecked={values.darkMode} onChange={updateCheckoxValue('darkMode')} />
                 <CheckboxInput
-                    label='Start on Login'
+                    label='use dark mode theme'
+                    id='darkMode'
+                    name='darkMode'
+                    defaultChecked={values.darkMode}
+                    onChange={updateCheckoxValue('darkMode')}
+                />
+                <CheckboxInput
+                    label='Start on login'
                     id='startOnLogin'
                     name='startOnLogin'
                     defaultChecked={values.startOnLogin}
                     onChange={updateCheckoxValue('startOnLogin')}
                 />
+
                 <CheckboxInput
-                    label='Shortcut to open the App'
+                    label='Shortcut to open the app'
                     id='openShortcutCheckbox'
                     name='openShortcutCheckbox'
                     defaultChecked={showShortcutInput}
                     onChange={handleOpenShortcutCheckboxClick}
                 />
-
                 {showShortcutInput && (
                     <FormInput
                         id='openShortcut'
                         name='openShortcut'
-                        placeholder='CmdOrCtrl+Shift+m'
+                        placeholder='e.g. CmdOrCtrl+Shift+m'
                         indented={4}
                         info={<ShortcutInfo />}
                         value={values.openShortcut}
                         onChange={updateTextValue('openShortcut')}
                     />
                 )}
+
+                <Box>
+                    <Label mb={1} fontWeight='bold' fontSize={1} htmlFor='trayIconForDarkMode'>
+                        Tray icon
+                    </Label>
+                    <Select
+                        id='trayIconForDarkMode'
+                        name='trayIconForDarkMode'
+                        defaultValue={values.trayIconForDarkMode}
+                        style={{ borderRadius: 0 }}
+                        onChange={updateTextValue('trayIconForDarkMode')}
+                    >
+                        <option value='system' key='system'>
+                            System Defaults
+                        </option>
+                        <option value='darkMode' key='darkMode'>
+                            Force dark mode
+                        </option>
+                        <option value='lightMode' key='lightMode'>
+                            Force light mode
+                        </option>
+                    </Select>
+                </Box>
             </form>
         </Box>
     )
