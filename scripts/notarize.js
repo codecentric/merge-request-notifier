d2d3ff82174c72a71e2598fbd0716967f2ebfdf0
Matthias Rütten <matthias.ruetten@codecentric.de>
2020-08-21 13:52:54 +0200

    fix the notarizing script

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -3,17 +3,18 @@ const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
     const { electronPlatformName, appOutDir } = context;
-    if (electronPlatformName === 'darwin') {
+    if (electronPlatformName !== 'darwin') {
         console.log(`notarizing the app is skipped because the electron platform name is "${electronPlatformName}"`)
         return;
     }
-    console.log('notarizing the app')
-
     const appName = context.packager.appInfo.productFilename;
+    const appPath = `${appOutDir}/${appName}.app`
+
+    console.log('notarizing the app', appPath)
 
     return await notarize({
         appBundleId: 'de.codecentric.ruettenm.mergeRequestNotifier',
-        appPath: `${appOutDir}/${appName}.app`,
+        appPath,
         appleId: process.env.APPLEID,
         appleIdPassword: process.env.APPLEIDPASS,
     });
