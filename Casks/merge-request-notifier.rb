4739d9a57efbe1be21d6a7cc30d980e681aacd82
Matthias Rütten <matthias.ruetten@codecentric.de>
2020-10-07 10:03:46 +0200

    Update Cask File

--- Casks/merge-request-notifier.rb
+++ Casks/merge-request-notifier.rb
@@ -1,6 +1,6 @@
 cask 'merge-request-notifier' do
-  version '1.8.3'
-  sha256 '2d1e2cac96f81564330486840f1136aaf668180c4683ea4a9b2115e275f546af'
+  version '1.8.4'
+  sha256 '227e7bb5d6ff02816ad65d3a475a7c4916e28f29ceabed237268aa79b141e889'
 
   url "https://github.com/codecentric/merge-request-notifier/releases/download/v#{version}/Merge-Request-Notifier-#{version}.dmg"
   appcast 'https://github.com/codecentric/merge-request-notifier/releases.atom'
