f53f2811bd80d830ed982e290f8392911a6947a2
Matthias Rütten <matthias.ruetten@codecentric.de>
2021-08-27 21:32:21 +0200

    remove "electron-util" and use "new-github-issue-url" instead

--- src/share/reportUnhandledRejections.ts
+++ src/share/reportUnhandledRejections.ts
@@ -1,14 +1,17 @@
 import unhandled from 'electron-unhandled'
-import { debugInfo, openNewGitHubIssue } from 'electron-util'
+import newGithubIssueUrl from 'new-github-issue-url'
+import { shell } from 'electron'
 
 export function reportUnhandledRejections() {
     unhandled({
-        reportButton: (error) => {
-            openNewGitHubIssue({
-                user: 'codecentric',
-                repo: 'merge-request-notifier',
-                body: `\`\`\`\n${error.stack}\n\`\`\`\n\n---\n\n${debugInfo()}`,
-            })
+        reportButton: async (error) => {
+            await shell.openExternal(
+                newGithubIssueUrl({
+                    user: 'codecentric',
+                    repo: 'merge-request-notifier',
+                    body: `Error: ${error.message}\n\n---\n\n\`\`\`\n${error.stack}\n\`\`\``,
+                }),
+            )
         },
     })
 }
